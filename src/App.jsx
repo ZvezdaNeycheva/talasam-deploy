77e389523cde79d875f3fdc5b7883205005b4814
Zvezda Neycheva <138030249+ZvezdaNeycheva@users.noreply.github.com>
2024-08-09 18:54:31 +0000

    fix: path

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/game/:page" element={<Game />} />
+        <Route path="/" element={<Game />} />
       </Routes>
     </BrowserRouter>
   );
