011fa14c40dec48dac5c9c9666c754103c69043e
Zvezda Neycheva <138030249+ZvezdaNeycheva@users.noreply.github.com>
2024-08-09 16:15:23 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css';
 import { Game } from './components/Game.jsx';
-// import { Header } from './components/Header.jsx';
 
 function App() {
 
   return (
     <>
-    {/* <Header></Header> */}
       <Game></Game>
     </>
   )
